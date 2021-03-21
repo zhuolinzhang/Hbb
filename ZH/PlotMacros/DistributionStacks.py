0c255365938ce81a8962399deb5395ec7521d92e
zhuolin <zzl0024@qq.com>
2021-03-22 00:40:53 +0800

    Update plot macros

--- ZH/PlotMacros/DistributionStacks.py
+++ ZH/PlotMacros/DistributionStacks.py
@@ -11,35 +11,49 @@ from FileIO import open_file, get_primary_name
 MCinfo_list = []
 factor = 1
 sample_kind_list = ['sig', 'st', 'tt', 'dib', 'qcd', 'zjets']
-hist_mu_list = ['RecDiMuon', 'RecMuonPlus', 'RecMuonMinus']
-selection_list = ['Tight', 'Tight_Trigger', 'Loose', 'Loose_Trigger']
+hist_mu_list = ['RecDiMuon', 'RecDiJet']
+mu_selection_list = ['Match']
 kinematic_list = ['pt', 'eta', 'phi']
 kine_hist_dict = {'pt':'50, 0, 500', 'eta':'60, -6, 6', 'phi':'40, -4, 4'} # set the paramameters of TH1
 match_dict = {'ZH_HToBB':'sig', 'TTTo':'tt', 'channel':'st', 'ZZ':'dib', 'QCD':'qcd', 'DYJetsToLL':'zjets'} # help categorize samples
 
+# Set bkg lists
+for i in mu_selection_list:
+    exec("bkg_list_RecDiMuon_{}_M = []".format(i))
+for i in mu_selection_list:
+    exec("bkg_list_RecDiJet_{}_M = []".format(i))
+for h in hist_mu_list:
+    for s in mu_selection_list:
+        for k in kinematic_list:
+            exec("bkg_list_{}_{}_{} = []".format(h, s, k))
+
 # Construct histograms to analyze
 for i in sample_kind_list:
-    for j in selection_list:
+    for j in mu_selection_list:
         exec('{0}_RecDiMuon_{1}_M = ROOT.TH1F("{0}_RecDiMuon_{1}_M", "", 60, 75, 105)'.format(i, j))
+for i in sample_kind_list:
+    for j in mu_selection_list:
+        exec('{0}_RecDiJet_{1}_M = ROOT.TH1F("{0}_RecDiJet_{1}_M", "", 75, 50, 200)'.format(i, j))
 
 for i in sample_kind_list:
     for j in hist_mu_list:
-        for s in selection_list:
+        for s in mu_selection_list:
             for k in kinematic_list:
                 for key, value in kine_hist_dict.items():
                     if key == k:
                         exec('{0}_{1}_{2}_{3} = ROOT.TH1F("{0}_{1}_{2}_{3}", "", {4})'.format(i, j, s, k, value))
 
 # Load infomation of MC samples
-with open('../Database/MCInfo.json') as MCinfo:
+with open('../../Database/MCInfo.json') as MCinfo:
     MCinfo_list = json.load(MCinfo)
 
 root_file_open = open_file('./Samples')
+
 data_file_open = open_file('./Data')
-data_hist_list = read_data(data_file_open, hist_mu_list, selection_list, kinematic_list, kine_hist_dict)
-# Plot invariant mass spectrum and distributions for data
+data_hist_list = read_data(data_file_open, hist_mu_list, mu_selection_list, kinematic_list, kine_hist_dict)
 for i in data_hist_list:
-    plot_hist(i)
+    plot_hist(i) 
+
 '''
 # Check the read_data function is right
 with open("entries.txt", 'w') as entries:
@@ -48,10 +62,10 @@ with open("entries.txt", 'w') as entries:
         hist_n = i.GetEntries()
         entries.write("Name: {}, N = {} \n".format(hist_name, hist_n))
 '''
-# Plot invariant mass spectrum
+# Plot invariant mass spectrum of dimuon
 for fileopenname in root_file_open:
-    for s in selection_list:
-        exec('h_RecDiMuon_{0}_M = fileopenname.Get("demo/h_RecDiMuon_{0}_M")'.format(s))
+    for s in mu_selection_list:
+        exec('h_RecDiMuon_{0}_M = fileopenname.Get("h_RecDiMuon_{0}_M")'.format(s))
     comparename = get_primary_name(root_file_open.index(fileopenname))
     
     # Read scale factor and check the factor for each dataset is right
@@ -65,25 +79,62 @@ for fileopenname in root_file_open:
                     break
     
     # Scale histograms, the scale factor is from MCInfo.json
-    for s in selection_list:
+    for s in mu_selection_list:
         exec("scale_hist(h_RecDiMuon_{0}_M, factor)".format(s))
     # Categorize MC samples
-    for s in selection_list:
+    for s in mu_selection_list:
         for key, value in match_dict.items():
             if re.search(key, comparename):
-                exec("{0}_RecDiMuon_{1}_M += h_RecDiMuon_{1}_M".format(value, s))
+                exec("{0}_RecDiMuon_{1}_M += h_RecDiMuon_{1}_M".format(value, s)) # e.g. sig_RecDiMuon_Trigger_M
+# Categorize bkg samples in a list
+for s in mu_selection_list:
+    for k in sample_kind_list:
+        if k == 'sig':
+            continue
+        exec("bkg_list_RecDiMuon_{1}_M.append({0}_RecDiMuon_{1}_M)".format(k, s))
+        
+# Plot invariant mass spectrum of DiJet
+for fileopenname in root_file_open:
+    for s in mu_selection_list:
+        exec('h_RecDiJet_{0}_M = fileopenname.Get("h_RecDiJet_{0}_M")'.format(s))
+    comparename = get_primary_name(root_file_open.index(fileopenname))
+    
+    # Read scale factor and check the factor for each dataset is right
+    for i in MCinfo_list:
+        for key, value in i.items():
+            if key == 'primary_name':
+                if value == comparename:
+                    factor = i['factor_2018']
+                    print("The primary name: ", value)
+                    print("The factor is ", factor)
+                    break
+    
+    # Scale histograms, the scale factor is from MCInfo.json
+    for s in mu_selection_list:
+        exec("scale_hist(h_RecDiJet_{0}_M, factor)".format(s))
+    # Categorize MC samples
+    for s in mu_selection_list:
+        for key, value in match_dict.items():
+            if re.search(key, comparename):
+                exec("{0}_RecDiJet_{1}_M += h_RecDiJet_{1}_M".format(value, s)) # e.g. sig_RecDiJet_Trigger_M
+
+for s in mu_selection_list:
+    for k in sample_kind_list:
+        if k == 'sig':
+            continue
+        exec("bkg_list_RecDiJet_{1}_M.append({0}_RecDiJet_{1}_M)".format(k, s))
 
 # Plot invariant mass specturm of MC samples
 for i in sample_kind_list:
-    for s in selection_list:
+    for s in mu_selection_list:
         exec('plot_hist({}_RecDiMuon_{}_M)'.format(i, s))
 
 # Plot distributions (pt, eta, phi)
 for fileopenname in root_file_open:
     for i in hist_mu_list:
-        for s in selection_list:
+        for s in mu_selection_list:
             for k in kinematic_list:
-                exec('h_{0}_{1}_{2} = fileopenname.Get("demo/h_{0}_{1}_{2}")'.format(i, s, k))
+                exec('h_{0}_{1}_{2} = fileopenname.Get("h_{0}_{1}_{2}")'.format(i, s, k))
     comparename = get_primary_name(root_file_open.index(fileopenname))
     
     # Read scale factor and check the factor for each dataset is right
@@ -98,30 +149,40 @@ for fileopenname in root_file_open:
     
     # Scale histograms, the scale factor is from MCInfo.json
     for i in hist_mu_list:
-        for s in selection_list:
+        for s in mu_selection_list:
             for k in kinematic_list:
                 exec("scale_hist(h_{}_{}_{}, factor)".format(i, s, k))
     # Categorize MC samples
     for i in hist_mu_list:
-        for s in selection_list:
+        for s in mu_selection_list:
             for k in kine_hist_dict:
                 for key, value in match_dict.items():
                     if re.search(key, comparename):
-                        exec("{0}_{1}_{2}_{3} += h_{1}_{2}_{3}".format(value, i, s, k))
+                        exec("{0}_{1}_{2}_{3} += h_{1}_{2}_{3}".format(value, i, s, k)) # sig_RecDiMuon_Trigger_M
+
+for s in mu_selection_list:
+    for h in hist_mu_list:
+        for i in kinematic_list:
+            for k in sample_kind_list:
+                if k == 'sig':
+                    continue
+                exec("bkg_list_{0}_{1}_{2}.append({3}_{0}_{1}_{2})".format(h, s, i, k))
 
 # Plot distributions of MC samples
 for i in sample_kind_list:
     for j in hist_mu_list:
-        for s in selection_list:
+        for s in mu_selection_list:
             for k in kinematic_list:
                 exec('plot_hist({}_{}_{}_{})'.format(i, j, s, k))
 
 # Plot the stack and Data/MC for invariant mass spectrum
-for s in selection_list:
-    exec('plot_ratio({1}_RecDiMuon_{0}_M, {2}_RecDiMuon_{0}_M, {3}_RecDiMuon_{0}_M, {4}_RecDiMuon_{0}_M, {5}_RecDiMuon_{0}_M, {6}_RecDiMuon_{0}_M, "RecDiMuon_{0}_M", data_hist_list)'.format(s, sample_kind_list[0], sample_kind_list[1], sample_kind_list[2], sample_kind_list[3], sample_kind_list[4], sample_kind_list[5]))
+for s in mu_selection_list:
+    exec('plot_ratio(sig_RecDiMuon_{0}_M, bkg_list_RecDiMuon_{0}_M, "RecDiMuon_{0}_M", data_hist_list)'.format(s))
+for s in mu_selection_list:
+    exec('plot_ratio(sig_RecDiJet_{0}_M, bkg_list_RecDiJet_{0}_M, "RecDiJet_{0}_M", data_hist_list)'.format(s))
 
 # Plot the stack and Data/MC for distributions
 for h in hist_mu_list:
-    for s in selection_list:
+    for s in mu_selection_list:
         for k in kinematic_list:
-            exec('plot_ratio({3}_{0}_{1}_{2}, {4}_{0}_{1}_{2}, {5}_{0}_{1}_{2}, {6}_{0}_{1}_{2}, {7}_{0}_{1}_{2}, {8}_{0}_{1}_{2}, "{0}_{1}_{2}", data_hist_list)'.format(h, s, k, sample_kind_list[0], sample_kind_list[1], sample_kind_list[2], sample_kind_list[3], sample_kind_list[4], sample_kind_list[5]))
+            exec('plot_ratio(sig_{0}_{1}_{2}, bkg_list_{0}_{1}_{2}, "{0}_{1}_{2}")'.format(h, s, k))
--- ZH/PlotMacros/lib/Plot.py
+++ ZH/PlotMacros/lib/Plot.py
@@ -5,6 +5,7 @@ import numpy as np
 def read_data(data_list, data_mu_list, data_selection_list, data_kine_list, data_kine_dict):
     for s in data_selection_list:
         exec('h_RecDiMuon_{0}_M = ROOT.TH1F("h_RecDiMuon_{0}_M", "h_RecDiMuon_{0}_M", 60, 75, 105)'.format(s))
+        exec('h_RecDiJet_{0}_M = ROOT.TH1F("h_RecDiJet_{0}_M", "h_RecDiJet_{0}_M", 75, 50, 200)'.format(s))
     for i in data_mu_list:
         for j in data_selection_list:
             for k in data_kine_list:
@@ -13,16 +14,24 @@ def read_data(data_list, data_mu_list, data_selection_list, data_kine_list, data
                         exec('h_{0}_{1}_{2} = ROOT.TH1F("h_{0}_{1}_{2}", "h_{0}_{1}_{2}", {3})'.format(i, j, k, value))
     for d in data_list:
         for s in data_selection_list:
-            exec('h_Get_RecDiMuon_{0}_M = d.Get("demo/h_RecDiMuon_{0}_M")'.format(s))
+            exec('h_Get_RecDiMuon_{0}_M = d.Get("h_RecDiMuon_{0}_M")'.format(s))
             exec('h_RecDiMuon_{0}_M += h_Get_RecDiMuon_{0}_M'.format(s))
+            exec('h_Get_RecDiJet_{0}_M = d.Get("h_RecDiJet_{0}_M")'.format(s))
+            exec('h_RecDiJet_{0}_M += h_Get_RecDiJet_{0}_M'.format(s))
         for i in data_mu_list:
             for j in data_selection_list:
                 for k in data_kine_list:
-                    exec('h_Get_{0}_{1}_{2} = d.Get("demo/h_{0}_{1}_{2}")'.format(i, j, k))
+                    exec('h_Get_{0}_{1}_{2} = d.Get("h_{0}_{1}_{2}")'.format(i, j, k))
                     exec('h_{0}_{1}_{2} += h_Get_{0}_{1}_{2}'.format(i, j, k))
     data_TH1F_list = []
+    # blind the signal region
+    for s in data_selection_list:
+        for i in range(20, 50):
+            exec('h_RecDiJet_{}_M.SetBinContent({}, 0)'.format(s, i))
+    
     for s in data_selection_list:
         exec('data_TH1F_list.append(h_RecDiMuon_{0}_M)'.format(s))
+        exec('data_TH1F_list.append(h_RecDiJet_{0}_M)'.format(s))
     for i in data_mu_list:
         for j in data_selection_list:
             for k in data_kine_list:
@@ -31,8 +40,9 @@ def read_data(data_list, data_mu_list, data_selection_list, data_kine_list, data
 
 def scale_hist(hist, scale_factor):
     # Normalize histograms to a specific integrated luminosity
+    scale_factor = scale_factor * 0.004536324 # correct to ref. trigger
     hist.Scale(scale_factor)
-    hist_entries = int(hist.GetEntries() * scale_factor)
+    hist_entries = hist.GetEntries() * scale_factor
     hist.SetEntries(hist_entries)
 
 def plot_hist(hist):
@@ -42,11 +52,20 @@ def plot_hist(hist):
     hist.Draw("HIST")
     c1.SaveAs("Figures/{}.pdf".format(hist.GetName()))
 
-def stack_fill(hist, color, stack):
+def stack_fill(hist, stack):
     # Fill histograms to THStack
-    hist.SetFillColor(color)
-    hist.SetLineColor(ROOT.kBlack)
-    stack.Add(hist)
+    color_dict = {'st': ROOT.kBlue, 'tt': ROOT.kGray + 2, 'dib': ROOT.kGreen + 1, 'qcd': ROOT.kViolet - 2, 'zjets': ROOT.kYellow + 1}
+    if re.search('sig', '{}'.format(hist)):
+        hist.SetFillColor(ROOT.kRed + 1)
+        hist.SetLineColor(ROOT.kBlack)
+        stack.Add(hist)
+    else:
+        for i in hist:
+            for key, value in color_dict.items():
+                if re.search(key, '{}'.format(i)):
+                    i.SetFillColor(value)
+            i.SetLineColor(ROOT.kBlack)
+            stack.Add(i)
 
 def make_ratio(hist_pass, hist_total, option = 'pois'):
     # make Data/MC graph and set the style of the graph
@@ -56,7 +75,7 @@ def make_ratio(hist_pass, hist_total, option = 'pois'):
     ratio.Draw("AP")
     ratio.GetYaxis().SetNdivisions(505)
     ratio.SetMaximum(2.49)
-    ratio.SetMinimum(0.)
+    ratio.SetMinimum(0.01)
     ratio.SetMarkerStyle(20)
     ratio.GetYaxis().SetTitleSize(0.08)
     ratio.GetXaxis().SetTitleSize(0.1)
@@ -65,15 +84,33 @@ def make_ratio(hist_pass, hist_total, option = 'pois'):
     ratio.GetYaxis().SetLabelSize(0.08)
     ratio.GetYaxis().SetTitleOffset(0.4)
     return ratio
+
+def make_legend(legend, hist_sig, hist_bkg_list, err_hist, data_hist = None):
+    # We must notice that the result of read_data is a list. Here we input data_hist which should be a TH1 histogram to this function.
+    bkg_leg_dict = {'st': "Single top", 'tt': "t#bar{t}", 'dib': "ZZ", 'qcd': "QCD", 'zjets': "Z+jets"}
+    if data_hist != None:
+        legend.AddEntry(data_hist, "Data", "P")
+    legend.AddEntry(hist_sig, "ZH(b#bar{b})", "F")
+    legend.AddEntry(err_hist, "MC Stat. Error", "F")
+    for i in hist_bkg_list:
+        for key, value in bkg_leg_dict.items():
+            if re.search(key, '{}'.format(i)):
+                legend.AddEntry(i, value, "F")
+    legend.SetNColumns(2)
+    legend.SetBorderSize(0)
     
-def plot_ratio(hist_sig, hist_bkg1, hist_bkg2, hist_bkg3, hist_bkg4, hist_bkg5, stack_name, data_TH1F_list):
+def plot_ratio(hist_sig, hist_bkg_list, stack_name, data_TH1F_list = None):
     # Plot the stack histogram and Data/MC
     # Sum MC samples and create MC Stat. Err. graphs
-    sum_hist = hist_sig + hist_bkg1 + hist_bkg2 + hist_bkg3 + hist_bkg4 + hist_bkg5
+    sum_hist = hist_sig
+    for i in hist_bkg_list:
+        sum_hist += i
     stack_err = ROOT.TGraphErrors(sum_hist) # MC. Stat. Err.
-    rel_err_hist = stack_err.Clone("rel_err_hist") # MC. relative Stat. Err.
-
+    #rel_err_hist = stack_err.Clone("rel_err_hist") # MC. relative Stat. Err.
+    rel_err_hist = ROOT.TGraphErrors(sum_hist.GetNbinsX())
+    rel_err_hist.SetTitle("")
     # Fill TGraphErrors(MC relative Stat. Err.)
+    '''
     for i in range(0, rel_err_hist.GetN()):
         n = i + 1
         point_y = rel_err_hist.GetPointY(n)
@@ -84,6 +121,18 @@ def plot_ratio(hist_sig, hist_bkg1, hist_bkg2, hist_bkg3, hist_bkg4, hist_bkg5, 
             rel_err_y = point_err_y / point_y
         rel_err_hist.SetPointY(n, 1.)
         rel_err_hist.SetPointError(n, rel_err_hist.GetErrorX(n), rel_err_y)
+    '''
+    for i in range(0, sum_hist.GetNbinsX()):
+        n = i + 1
+        point_y = sum_hist.GetBinContent(n)
+        point_err_y = sum_hist.GetBinError(n)
+        if point_y == 0:
+            rel_err_y = 0
+        else:
+            rel_err_y = point_err_y / point_y
+        rel_err_hist.SetPoint(n, sum_hist.GetXaxis().GetBinCenter(n), 1.)
+        rel_err_hist.SetPointError(n, sum_hist.GetBinWidth(n) / 2, rel_err_y)
+
     
     # Create TCanvas and two TPads to place THStack and Data/MC
     c2 = ROOT.TCanvas("c2", "", 800, 600)
@@ -96,31 +145,30 @@ def plot_ratio(hist_sig, hist_bkg1, hist_bkg2, hist_bkg3, hist_bkg4, hist_bkg5, 
     upperPad.SetBottomMargin(0.)
     upperPad.cd() # active uppper pad
     # Fill THStack and set the style of THStack
-    hs = ROOT.THStack("hs","{}".format(stack_name))
-    stack_fill(hist_sig, ROOT.kYellow + 1, hs)
-    stack_fill(hist_bkg1, ROOT.kBlue, hs)
-    stack_fill(hist_bkg2, ROOT.kGray + 2, hs)
-    stack_fill(hist_bkg3, ROOT.kGreen + 1, hs)
-    stack_fill(hist_bkg4, ROOT.kViolet - 2, hs)
-    stack_fill(hist_bkg5, ROOT.kRed + 1, hs)
+    hs = ROOT.THStack("hs","")
+    stack_fill(hist_sig, hs)
+    stack_fill(hist_bkg_list, hs)
     hs.Draw("HIST")
-    hs.SetMinimum(1e-1)
-    hs.SetMaximum(3e8)
+    hs.SetMinimum(1e-3)
+    hs.SetMaximum(3e3)
     hs.GetYaxis().SetTitleOffset(1)
     hs.GetYaxis().SetTitle("Events / {:.1f}".format(sum_hist.GetXaxis().GetBinWidth(1)))
 
     # Plot the MC Stat. Err. and set the style of MC Stat. Err.
     stack_err.SetFillStyle(3013)
     stack_err.SetFillColor(ROOT.kBlack)
+    stack_err.SetLineColor(ROOT.kBlack)
     stack_err.Draw("2")
 
     # Plot data points
-    for i in data_TH1F_list:
-        if re.search(stack_name, i.GetName()):
-            data_hist = i
-    data_hist.SetMarkerStyle(20)
-    data_hist.SetStats(0) # avoid the statistics box is drawn after plot_hist the data histograms
-    data_hist.Draw("SAME E0")
+    if data_TH1F_list != None:
+        for i in data_TH1F_list:
+            if re.search(stack_name, i.GetName()):
+                data_hist = i
+        data_hist.SetMarkerStyle(20)
+        data_hist.SetStats(0) # avoid the statistics box is drawn after plot_hist the data histograms
+        data_hist.SetLineColor(ROOT.kBlack)
+        data_hist.Draw("SAME E0")
     '''
     # Check the match of THStack and data are right
     print("------------------")
@@ -131,23 +179,18 @@ def plot_ratio(hist_sig, hist_bkg1, hist_bkg2, hist_bkg3, hist_bkg4, hist_bkg5, 
 
     # Set the legend of THStack
     leg = ROOT.TLegend(0.63, 0.70, 0.86, 0.87)
-    leg.AddEntry(data_hist, "Data", "P")
-    leg.AddEntry(hist_bkg5, "Z+jets", "F")
-    leg.AddEntry(hist_bkg4, "QCD", "F")
-    leg.AddEntry(hist_bkg3, "ZZ", "F")
-    leg.AddEntry(hist_bkg1, "Single top", "F")
-    leg.AddEntry(hist_bkg2, "t#bar{t}", "F")
-    leg.AddEntry(hist_sig, "ZH(b#bar{b})", "F")
-    leg.AddEntry(stack_err, "MC Stat. Error", "F")
-    leg.SetNColumns(2)
-    leg.SetBorderSize(0)
+    make_legend(leg, hist_sig, hist_bkg_list, stack_err)
     leg.Draw("SAME")
 
     # Set the CMS label, collision energy and integrated luminosity
     fig_lable = ROOT.TLatex()
     fig_lable.SetTextSize(.03)
-    fig_lable.DrawLatexNDC(.7, .91, "#sqrt{s} = 13 TeV, L = 59.74 fb^{-1}")
-    fig_lable.DrawLatexNDC(.1, .91, "#scale[1.2]{CMS} #font[52]{Preliminary}")
+    data_lumi = 0.271
+    fig_lable.DrawLatexNDC(.75, .91, "#font[42]{13 TeV (%s fb^{-1})}" % data_lumi)
+    if data_TH1F_list != None:
+        fig_lable.DrawLatexNDC(.1, .91, "#scale[1.2]{CMS} #font[52]{Preliminary}")
+    else:
+        fig_lable.DrawLatexNDC(.1, .91, "#scale[1.2]{CMS} #font[52]{Simulation Preliminary}")
 
     # Set the style of lower pad (MC relative Stat. Err. and Data/MC)
     lowerPad.cd() # active lower pad
@@ -155,18 +198,55 @@ def plot_ratio(hist_sig, hist_bkg1, hist_bkg2, hist_bkg3, hist_bkg4, hist_bkg5, 
     lowerPad.SetGridy()
     lowerPad.SetTopMargin(0.)
     lowerPad.SetBottomMargin(0.3)
-    ratio = make_ratio(data_hist, sum_hist)
-    ratio.GetXaxis().SetLimits(data_hist.GetXaxis().GetXmin(), data_hist.GetXaxis().GetXmax())
-    rel_err_hist.SetFillStyle(3013)
-    rel_err_hist.SetFillColor(ROOT.kBlack)
-    rel_err_hist.Draw("2")
-    if re.search('_M', stack_name):
-        ratio.GetXaxis().SetTitle("m_{#mu^{+}#mu^{-}} [GeV]")
-    if re.search('pt', stack_name):
-        ratio.GetXaxis().SetTitle("p_{T} [GeV]")
-    if re.search('eta', stack_name):
-        ratio.GetXaxis().SetTitle("#eta")
-    if re.search('phi', stack_name):
-        ratio.GetXaxis().SetTitle("#phi")
+    if data_TH1F_list != None:
+        ratio = make_ratio(data_hist, sum_hist)
+        ratio.GetXaxis().SetLimits(data_hist.GetXaxis().GetXmin(), data_hist.GetXaxis().GetXmax())
+        rel_err_hist.SetFillStyle(3013)
+        rel_err_hist.SetFillColor(ROOT.kBlack)
+        rel_err_hist.Draw("2")
+    else:
+        ratio = rel_err_hist
+        ratio.GetXaxis().SetLimits(sum_hist.GetXaxis().GetXmin(), sum_hist.GetXaxis().GetXmax())
+        ratio.SetFillStyle(3013)
+        ratio.SetFillColor(ROOT.kBlack)
+        ratio.GetYaxis().SetTitle("Relative Errors")
+        ratio.Draw("A2")
+        ratio.GetYaxis().SetNdivisions(505)
+        ratio.SetMaximum(2.49)
+        ratio.SetMinimum(0.01)
+        ratio.SetMarkerStyle(20)
+        ratio.GetYaxis().SetTitleSize(0.08)
+        ratio.GetXaxis().SetTitleSize(0.1)
+        ratio.GetXaxis().SetLabelSize(0.08)
+        ratio.GetXaxis().SetTitleOffset(1)
+        ratio.GetYaxis().SetLabelSize(0.08)
+        ratio.GetYaxis().SetTitleOffset(0.4)
     
+    if re.search("DiJet", stack_name):
+        if re.search('_M', stack_name):
+            ratio.GetXaxis().SetTitle("m_{DiJet} [GeV]")
+        if re.search('pt', stack_name):
+            ratio.GetXaxis().SetTitle("p_{T}^{DiJet} [GeV]")
+        if re.search('eta', stack_name):
+            ratio.GetXaxis().SetTitle("#eta_{DiJet}")
+        if re.search('phi', stack_name):
+            ratio.GetXaxis().SetTitle("#phi_{DiJet}")
+    elif re.search("DiMuon", stack_name):
+        if re.search('_M', stack_name):
+            ratio.GetXaxis().SetTitle("m_{DiMuon} [GeV]")
+        if re.search('pt', stack_name):
+            ratio.GetXaxis().SetTitle("p_{T}^{DiMuon} [GeV]")
+        if re.search('eta', stack_name):
+            ratio.GetXaxis().SetTitle("#eta_{DiMuon}")
+        if re.search('phi', stack_name):
+            ratio.GetXaxis().SetTitle("#phi_{DiMuon}")
+    else:
+        if re.search('_M', stack_name):
+            ratio.GetXaxis().SetTitle("m [GeV]")
+        if re.search('pt', stack_name):
+            ratio.GetXaxis().SetTitle("p_{T} [GeV]")
+        if re.search('eta', stack_name):
+            ratio.GetXaxis().SetTitle("#eta")
+        if re.search('phi', stack_name):
+            ratio.GetXaxis().SetTitle("#phi")
     c2.SaveAs("Stacks/stack_{}.pdf".format(stack_name))
