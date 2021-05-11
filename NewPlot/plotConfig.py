06b32490a013b55ab1cb004752d1a6a4ed62375c
zhuolin <zzl0024@qq.com>
2021-05-11 11:13:26 +0800

    update new plot macro

--- NewPlot/plotConfig.py
+++ NewPlot/plotConfig.py
@@ -3,11 +3,11 @@ import ROOT
 import json
 
 class Hist(ROOT.TH1):
-    def __init__(self, name, phyObject, selection, dataset, tree):
-        self.name = name
+    def __init__(self, kinematics, phyObject, selection, dataset, tree):
+        self.kinematics = kinematics
         self.phyObject = phyObject
         self.selection = selection
-        self.histName = "{}_{}_{}".format(phyObject, selection, name)
+        self.histName = "h_{}_{}_{}".format(phyObject, selection, kinematics)
         self.dataset = dataset
         self.tree = tree
     
@@ -15,20 +15,27 @@ class Hist(ROOT.TH1):
         histParasDict = {'M': {'RecDiMuon': '60, 75, 105', 'RecDiJet': '75, 50, 200'}, 'Pt':'50, 0, 500', 'Eta':'60, -6, 6', 'Phi':'40, -4, 4'}
         loopFlag = False
         for kinimatic, para in histParasDict.items():
-            if kinimatic == self.name:
+            if kinimatic == self.kinematics:
                 loopFlag = True
                 histEdge = para
-                if self.name == 'M':
+                if self.kinematics == 'M':
                     histEdge = para[self.phyObject]
         if loopFlag == False: 
             print('You input wrong physical object!')
         self.histPara = histEdge
     
     def getScaleFactor(self):
-        with open('MCInfo.json') as mcInfo:
+        with open('MCInfo_IsoMu20.json') as mcInfo:
             mcInfoList = json.load(mcInfo)
+        factor = 1
+        for eachDataset in mcInfoList:
+            for value in eachDataset.values():
+                if self.dataset == value:
+                    factor = eachDataset["factor_IsoMu20"]
+                    break
+        if factor == 1: print("Your dataset name is not in the json file!")
+        return factor
 
-        
     def getHist(self):
         self.tree.Draw("{} >> {}({})") # nbins, xmin, xmax in the bracket
         h = ROOT.gROOT.FindObject()
