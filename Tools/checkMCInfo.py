e527ce26bcacc26b3502542930a5886f8416335f
zhuolinzhang <zzl0024@qq.com>
2021-05-30 02:13:57 +0800

    Move MC to UL2018

--- Tools/checkMCInfo.py
+++ Tools/checkMCInfo.py
@@ -1,37 +1,59 @@
-# This script will check the number of events for each dataset and correct it. Then, it will calculate the right normalization factor
-# This script MUST be run on the LXPLUS!!!
+# This script will 
+# 1 - check the number of events for each dataset and correct it. Then, it will calculate the right normalization factor
+# 2 - check the primary name
+# This script MUST be run on the LXPLUS or lxslc(IHEP)
 
+import argparse
 import os
 import json
 
-database_list = []
-with open("MCInfo.json", 'r') as database_json:
-    database_list = json.load(database_json)
+parse = argparse.ArgumentParser()
+parse.add_argument("--lumi", type=float, default=59.83, help="The integrated luminosity of data (/fb)")
+parse.add_argument("-i", type=str, help="The path of input json file")
+parse.add_argument("-o", type=str, default=None, help="The path of output json file, the default path is same as the input path")
+args = parse.parse_args()
 
-lumi_2018data = 59.74
-event = 1
+def checkVO():
+    if os.path.exists("/tmp/x509up_u12918"):
+        pass
+    else:
+        print("Please activate your VO!")
+        while not(os.path.exists("/tmp/x509up_u12918")):
+            os.system("voms-proxy-init -voms cms")
 
-for dataset_dict in database_list:
-    for key, value in dataset_dict.items():
+checkVO()
+databaseList = []
+newDatabaseList = []
+with open(args.i, 'r') as databaseJson:
+    databaseList = json.load(databaseJson)
+
+for datasetDict in databaseList:
+    newDatasetDict = datasetDict
+    nEvents = datasetDict["nevents"]
+    print("Read {}".format(datasetDict["primary_name"]))
+    for key, value in datasetDict.items():
+        if key == "primary_name":
+            newPrimaryName = datasetDict["dasname"].split('/')[1]
+            if value == newPrimaryName: pass
+            else: 
+                print("The primary_name is wrong. Correct it!")
+                newDatasetDict["primary_name"] = newPrimaryName
         if key == "dasname":
-            os.system('dasgoclient -query="dataset=%s" -json > datainfo.json' % value)
-            with open("datainfo.json", 'r') as das_json:
-                das_info = json.load(das_json)
-            for dict_ele in das_info:
-                for key, value in dict_ele["das"].items():
-                    if key == "services":
-                        for i in value:
-                            if i == "dbs3:filesummaries":
-                                for j in dict_ele["dataset"]:
-                                    for key, value in j.items():
-                                        if key == "nevents":
-                                            event = value
-                                    break
-        if dataset_dict["nevents"] == event:
+            f = os.popen('dasgoclient -query="summary dataset={}"'.format(value))
+            dasSummary = f.readline()
+            dasSummaryList = dasSummary.split(",")
+            for i in dasSummaryList:
+                if "nevents" in i:
+                    nEvents = int(i.split(":")[-1])
+        if datasetDict["nevents"] == nEvents:
             continue
         else:
-            dataset_dict["nevents"] = event
-            dataset_dict["factor_2018"] = lumi_2018data / ((event / dataset_dict["xsection"]) / 1000)
-
-with open("MCInfo.json", 'w') as new_data_json:
-    json.dump(database_list, new_data_json, indent=4)
+            print("The nevnets is wrong!\tlocal = {}\tdas = {}".format(datasetDict["nevents"], nEvents))
+            newDatasetDict["nevents"] = nEvents
+            newDatasetDict["factor_IsoMu20"] = args.lumi / ((nEvents / datasetDict["xsection"]) / 1000)
+    newDatabaseList.append(newDatasetDict)
+        
+outputPath = args.o
+if args.o == None: outputPath = args.i
+with open(outputPath, 'w') as newJson:
+    json.dump(newDatabaseList, newJson, indent=4)
